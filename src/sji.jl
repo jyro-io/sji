f3b0ba3b6abeb48a2d16ef19fd49e333e0f481e2
Rory Linehan <rorylinehan@protonmail.com>
2023-01-15 20:09:48 -0600

    change variable

--- src/sji.jl
+++ src/sji.jl
@@ -462,7 +462,7 @@ function convert_ohlc_interval(data::DataFrame, time::String, destination::OHLCI
   interval = get_ohlc_interval_method(destination)
   is = 0  # interval size
   for (i, row) in enumerate(eachrow(data))
-    if >(r[p["time_field"]], d[begin, p["time_field"]] + interval)
+    if >(r[time], d[begin, time] + interval)
       is = i  # interval size
       break
     end
