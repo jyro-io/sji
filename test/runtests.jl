bde47e05e9935f5096a41875c99b3b31df6b993a
Rory Linehan <rorylinehan@protonmail.com>
2025-01-31 22:20:06 -0600

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,16 @@ using JSON
 using DataFrames
 
 @testset "sji.jl" begin
+  data = DataFrame(
+    :close => rand(Int, 100)
+  )
   result = sji.exponential_moving_average!(
-    DataFrame(
-      :close => rand(Int, 100)
-    ),
+    data,
     3,
     "close"
   )
   @debug "result" result
   result::AbstractDataFrame
   hasproperty(result, :ema_3)
+  @test length(result.ema_3) == length(data[:, :close])
 end
